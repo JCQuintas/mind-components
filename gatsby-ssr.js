b082d4223026df87c6330bee26db4929c1e18cc6
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-16 00:48:36 +0200

    Fix build - SSR needed styled provider

---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+// custom typefaces
+import 'typeface-ubuntu'
+import 'typeface-roboto'
+import React from 'react'
+import { Base } from './src/components/base'
+
+export const wrapRootElement = ({ element }) => <Base>{element}</Base>
