cb3d6fee2f77a7b33f95ef9d5bccc11cebe770bc
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-18 18:06:50 +0200

    Fix unstyled blink

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from 'react'
+// import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { ThemeModeProvider } from '../utils/theme'
 import { GlobalStylesComponent } from '../utils/global-style'
@@ -7,11 +8,20 @@ const StyledRoot = styled.div`
   height: 100%;
 `
 
-export const Base: FunctionComponent = props => (
-  <ThemeModeProvider>
-    <StyledRoot>
-      <GlobalStylesComponent />
-      {props.children}
-    </StyledRoot>
-  </ThemeModeProvider>
-)
+export const Base: FunctionComponent = props => {
+  const [render, setRender] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setRender(true), 100)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <ThemeModeProvider>
+      <StyledRoot>
+        <GlobalStylesComponent />
+        {render && props.children}
+      </StyledRoot>
+    </ThemeModeProvider>
+  )
+}
