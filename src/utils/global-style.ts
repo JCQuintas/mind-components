ac15dc7e8992e28933c529431933cda5da738261
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-21 02:43:47 +0200

    Better blockquote styles and cite

--- src/utils/global-style.ts
+++ src/utils/global-style.ts
@@ -3,7 +3,8 @@ import { createElement } from 'react'
 import { codeStyle } from './code-style'
 
 const GlobalStyle = createGlobalStyle`
-  html, body {
+  html,
+  body {
     height: 100%;
   }
 
@@ -42,12 +43,19 @@ const GlobalStyle = createGlobalStyle`
     color: ${({ theme }: Styled) => theme.palette.primary.color};
   }
 
-  h2, h3, h4, h5 {
+  h2,
+  h3,
+  h4,
+  h5 {
     color: ${({ theme }: Styled) => theme.palette.foreground.color};
     margin-top: ${({ theme }: Styled) => theme.typography.rhythm(2)};
   }
 
-  h1, h2, h3, h4, h5 {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
     transition: ${({ theme }: Styled) => theme.transition('color')};
 
     a {
@@ -64,7 +72,8 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${({ theme }: Styled) => theme.palette.foreground.opacity(0.1)};
   }
 
-  th, td {
+  th,
+  td {
     border-color: ${({ theme }: Styled) => theme.palette.foreground.opacity(0.1)};
   }
 
@@ -73,13 +82,53 @@ const GlobalStyle = createGlobalStyle`
   }
 
   blockquote {
-    padding:${({ theme }: Styled) => theme.typography.rhythm(1 / 2, 1)};
-    border-radius: ${({ theme }: Styled) => theme.typography.rhythm(1 / 2)};
-    margin-left:${({ theme }: Styled) => theme.typography.rhythm(1)};
+    position: relative;
+    padding: ${({ theme }: Styled) => theme.typography.rhythm(1 / 2, 1)};
+    border-bottom-left-radius: ${({ theme }: Styled) => theme.typography.rhythm(1 / 2)};
+    border-bottom-right-radius: ${({ theme }: Styled) => theme.typography.rhythm(1 / 2)};
+    margin-left: ${({ theme }: Styled) => theme.typography.rhythm(1 / 2)};
+    margin-left: ${({ theme }: Styled) => theme.typography.rhythm(1)};
     margin-right: ${({ theme }: Styled) => theme.typography.rhythm(1)};
     background: ${({ theme }: Styled) => theme.palette.foreground.opacity(0.1)};
-    border-left: 2px solid ${({ theme }: Styled) => theme.palette.primary.color};
-    border-right: 2px solid ${({ theme }: Styled) => theme.palette.primary.color};
+    border-left: 2px solid;
+    border-right: 2px solid;
+    border-image: linear-gradient(
+        to bottom,
+        ${({ theme }: Styled) => theme.palette.primary.color} 0%,
+        ${({ theme }: Styled) => theme.palette.primary.opacity(0)} 70%
+      )
+      1 100%;
+
+    &.right,
+    &.left {
+      ${({ theme }: Styled) => theme.typography.scale(0.3)};
+      max-width: ${({ theme }: Styled) => theme.typography.rhythm(10)};
+      line-height: ${({ theme }: Styled) => theme.typography.rhythm(1.3)};
+      word-spacing: ${({ theme }: Styled) => theme.typography.rhythm(0.1)};
+      letter-spacing: ${({ theme }: Styled) => theme.typography.rhythm(0.01)};
+    }
+
+    &.right {
+      float: right;
+    }
+
+    &.left {
+      float: left;
+    }
+
+    cite {
+      ${({ theme }: Styled) => theme.typography.scale(0)};
+      position: absolute;
+      color: ${({ theme }: Styled) => theme.palette.primary.color};
+      bottom: -1em;
+      right: ${({ theme }: Styled) => theme.typography.rhythm(1)};
+
+      &:before {
+        content: '-';
+        position: absolute;
+        left: -0.5em;
+      }
+    }
   }
 
   ${codeStyle}
