e870705356c5cfca296dccb7401dde6f4e253441
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-29 00:22:32 +0200

    Add new icon and automatic deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#78bb1b`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/mindcomponents-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
