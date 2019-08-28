1aa6489d39374858d41f0ae3190d4f1f03503f48
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-28 10:59:04 +0200

    Add analytics track id

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-146621193-1`,
       },
     },
     {
