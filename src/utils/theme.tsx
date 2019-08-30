6f876ce4ec44aa5d9c2aff452e4c778eb59fc440
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-31 00:07:42 +0200

    Update theme comments

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -99,16 +99,18 @@ export const lightTheme: Theme = {
 export const darkModeKey = `${process.env.GATSBY_PROJECT_ID}_IS_DARK_MODE`
 
 // Get dark mode setting from LocalStorage
+// We don't parse it into boolean because it's also important to know if it was not set
 const savedIsDarkMode = () => typeof window !== 'undefined' && `${localStorage.getItem(darkModeKey)}`
-// Check if browser prefers Dark Mode
-const supportsDarkMode = () =>
+
+// Check if browser prefers Dark Mode, default to dark in case window is undefined
+const prefersDarkMode = () =>
   typeof window !== 'undefined' ? matchMedia('(prefers-color-scheme: dark)').matches : true
 
 // Get usable isDarkMode
 export const getIsDarkMode = (): boolean =>
   // Checks if user has a preference already set and use it
   // If not set, then try to select mode by detecting system preference
-  savedIsDarkMode() === 'true' ? true : savedIsDarkMode() === 'false' ? false : supportsDarkMode()
+  savedIsDarkMode() === 'true' ? true : savedIsDarkMode() === 'false' ? false : prefersDarkMode()
 
 // The ThemeModeContext can be imported and used on useContext
 // in order to use setIsDarkMode from within the app
