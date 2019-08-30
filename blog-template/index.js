b284c61ac6553ac912285aa7c48aaea2e3e9a8d5
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-30 16:45:07 +0200

    Create cli to generate blog post

---
+++ blog-template/index.js
@@ -0,0 +1,55 @@
+const { existsSync, mkdirSync, readFileSync, writeFileSync } = require('fs')
+const { join } = require('path')
+const inquirer = require('inquirer')
+const { paramCase, titleCase, upperCaseFirst } = require('change-case')
+
+const red = '\x1b[31m'
+const reset = '\x1b[0m'
+const green = '\x1b[32m'
+
+const createDirectoryIfNotExists = dir => {
+  if (!existsSync(dir)) {
+    mkdirSync(dir)
+  }
+}
+
+const BLOG_TEMPLATE_FOLDER = './blog-template'
+const BLOG_CONTENT_FOLDER = '../content/blog'
+const TITLE_REPLACE = '<TITLE>'
+const DESCRIPTION_REPLACE = '<DESCRIPTION>'
+
+const start = async () => {
+  const postData = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Title for the blog post:',
+      validate: Boolean,
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Description:',
+      default: 'No description yet!',
+    },
+  ])
+
+  const directory = paramCase(postData.title)
+  const directoryPath = join(__dirname, BLOG_CONTENT_FOLDER, directory)
+
+  if (existsSync(directoryPath)) console.log(red + `A post or folder '${directory}' already exists.` + reset)
+
+  createDirectoryIfNotExists(directoryPath)
+
+  const file = readFileSync(join(__dirname, 'index.md'), { encoding: 'utf8' })
+  await writeFileSync(
+    join(directoryPath, 'index.md'),
+    file
+      .replace(TITLE_REPLACE, titleCase(postData.title))
+      .replace(DESCRIPTION_REPLACE, upperCaseFirst(postData.description))
+  )
+
+  console.log(green + '✔' + reset + ' Everything done!')
+}
+
+start()
--- blog-template/index.md
+++ blog-template/index.md
@@ -1,6 +1,6 @@
 ---
-title: 'Post Title'
+title: '<TITLE>'
 created: '1970-01-01T00:00:00.000Z'
 edited: '1970-01-01T00:00:00.000Z'
-description: 'Post description.'
+description: '<DESCRIPTION>'
 ---
