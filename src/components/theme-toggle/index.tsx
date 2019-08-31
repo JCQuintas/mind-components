f27e2cad89854558cda438bfda2277ad2950c9a8
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-08-31 22:35:43 +0200

    Fix toggle issue hydration issue when built

--- src/components/theme-toggle/index.tsx
+++ src/components/theme-toggle/index.tsx
@@ -1,13 +1,21 @@
-import React, { FunctionComponent, useContext } from 'react'
+import React, { FunctionComponent, useContext, useState, useEffect } from 'react'
 import { ThemeContext } from 'styled-components'
 import { ThemeModeContext } from '../../utils/theme'
 import { Toggle } from './toggle'
 
 export const ThemeToggle: FunctionComponent = () => {
+  const [render, setRender] = useState(false)
   const { setIsDarkMode } = useContext(ThemeModeContext)
   const theme = useContext<Theme>(ThemeContext)
 
   const handleChange = () => setIsDarkMode(s => !s)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setRender(true), 100)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  if (!render) return null
 
   return <Toggle checked={theme.isDark} onChange={handleChange} />
 }
